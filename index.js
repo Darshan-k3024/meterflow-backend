c921d7b981f90627dafdaa2699b52cb5df6cf758
darshan <darshankumbhar84@gmail.com>
2026-05-02 17:37:34 +0530

    fixed cors

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}))
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
